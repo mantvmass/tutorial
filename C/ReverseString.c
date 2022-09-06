f404c4040d31c7b07196c1922b8a8fcb0025fbb7
Phumin Maliwan <32133224+mantvmass@users.noreply.github.com>
2022-09-07 03:54:23 +0700

    Reverse charecter with linked_list and stack

---
+++ C/ReverseString.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<string.h>
+
+
+// void reverse_str(char *);
+
+
+int main(){
+
+    char message[60]; 
+    printf("Please insert the string you wish to get reversed: ");
+    scanf("%s", message);
+
+   
+
+    
+
+    return 0;
+}
+
+
+
+// void reverse_str(char data[]){
+//     char result[500];
+//     int count_data = count(data);
+//     for (int i = 0; i < count(data); i++, count_data--){
+//         printf("%c,", data[count_data]);
+//     }
+    
+// }
+
+
+
