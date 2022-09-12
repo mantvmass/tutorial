ab7905bb82a4613439b7c5218be7852e81eaa5a4
Phumin Maliwan <32133224+mantvmass@users.noreply.github.com>
2022-09-12 09:28:32 +0700

    use circle file

--- Java/testSpace.java
+++ Java/testSpace.java
@@ -1,8 +1,8 @@
-import space.circle; // file space/cirvle.java
+import space.circle; // file space/circle.java
 
 public class testSpace {
     public static void main(String[] args) {
-        // circle c1 = new circle(12.00f); use with constucture
+        // circle c1 = new circle(12.00f); use with constructor
         circle c1 = new circle();
         c1.r = 10;
         System.out.println(c1.area());
